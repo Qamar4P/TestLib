bad317651b4b21a4f1c632b4f56cc1b13e4cb650
Qamar4P(ツ) <qamar.dev@gmail.com>
2019-12-18 16:36:30 +0500

    asdf fix

--- app2/src/main/java/com/irisvision/libraryt/MainActivity.java
+++ app2/src/main/java/com/irisvision/libraryt/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //fix
+
        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
